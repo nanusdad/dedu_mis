b594d6b31f2e531b1e4fce3f2be33b99dca5d2ab
nanusdad <arun@zechariah.net>
2018-04-02 20:52:50 +0530

    updated genders

--- orion/collections/students.js
+++ orion/collections/students.js
@@ -6,25 +6,43 @@
  * we will show in the index of the collection in the admin
  */
 Students = new orion.collection('students', {
-  singularName: orion.helpers.getTranslation('students.singularName'), // The name of one of this items
-  pluralName: orion.helpers.getTranslation('students.pluralName'), // The name of more than one of this items
-  title: orion.helpers.getTranslation('students.title'), // The title of the page
-  link: {
-    /**
-     * The text that you want to show in the sidebar.
-     * The default value is the name of the collection, so
-     * in this case is not necesary
-     */
-    title: orion.helpers.getTranslation('students.title')
-  },
-  /**
-   * Tabular settings for this collection
-   */
-  tabular: {
-    columns: [
-      { data: 'title', title: orion.helpers.getTranslation('students.schema.title') },
-			{ data: 'sname', title: orion.helpers.getTranslation('students.schema.sname') },
-			{ data: 'gender', title: orion.helpers.getTranslation('students.schema.gender') },
+	singularName: orion.helpers.getTranslation('students.singularName'), // The name of one of this items
+	pluralName: orion.helpers.getTranslation('students.pluralName'), // The name of more than one of this items
+	title: orion.helpers.getTranslation('students.title'), // The title of the page
+	link: {
+		/**
+		 * The text that you want to show in the sidebar.
+		 * The default value is the name of the collection, so
+		 * in this case is not necesary
+		 */
+		title: orion.helpers.getTranslation('students.title')
+	},
+	/**
+	 * Tabular settings for this collection
+	 */
+	tabular: {
+		columns: [
+			{
+				data: 'title',
+				title: orion.helpers.getTranslation('students.schema.title')
+			},
+			{
+				data: 'sname',
+				title: orion.helpers.getTranslation('students.schema.sname')
+			},
+			{
+				data: 'gender',
+				title: orion.helpers.getTranslation('students.schema.gender')
+			},
+//			{
+//				dropdown: {
+//					type: String,
+//					label: "Select One",
+//					autoform: {
+//						options: /*....*/
+//					}
+//				}
+//			}
       /**
        * If you want to show a custom orion attribute in
        * the index table you must call this function
@@ -35,7 +53,7 @@ Students = new orion.collection('students', {
       orion.attributeColumn('createdBy', 'createdBy', orion.helpers.getTranslation('students.schema.createdBy')),
       orion.attributeColumn('createdAt', 'createdAt', orion.helpers.getTranslation('students.schema.createdAt'))
     ]
-  }
+	}
 });
 
 /**
@@ -43,52 +61,64 @@ Students = new orion.collection('students', {
  * Orion will automatically create the corresponding form.
  */
 Students.attachSchema(new SimpleSchema({
-  title: {
-    type: String,
-    label: orion.helpers.getTranslation('students.schema.title') // We use this function to make i18n work in autoform
-  },
+	title: {
+		type: String,
+		label: orion.helpers.getTranslation('students.schema.title') // We use this function to make i18n work in autoform
+	},
 	sname: {
-    type: String,
-    label: orion.helpers.getTranslation('students.schema.sname') // We use this function to make i18n work in autoform
-  },
+		type: String,
+		label: orion.helpers.getTranslation('students.schema.sname') // We use this function to make i18n work in autoform
+	},
 	gender: {
-    type: String,
-    label: orion.helpers.getTranslation('students.schema.gender') // We use this function to make i18n work in autoform
-  },
-  /*
-  We add relationship to cities here
-   */
-  
-  area: orion.attribute('hasOne', {
-    label: 'Course Enrolled'
-  }, {
-    collection: Courses,
-    titleField: 'course_name',
-    publicationName: 'postCourses',
+		//type: String,
+		//label: orion.helpers.getTranslation('students.schema.gender') // We use this function to make i18n work in autoform
+		//label: "Select One",
 
-    create: function (input, cb) {
-    var city = Course.insert({name: input, published: false});
-    var newCity = Courses.findOne(course);
-    cb(newCourse);
-    }
+			type: String,
+			label: orion.helpers.getTranslation('students.schema.gender'), // We use this function to make i18n work in autoform
+			allowedValues: [
+				orion.helpers.getTranslation('genderAllowedValues.male'),
+				orion.helpers.getTranslation('genderAllowedValues.female'),
+				orion.helpers.getTranslation('genderAllowedValues.trans')
+			]
+	},
+	/*
+	We add relationship to cities here
+	 */
+
+	area: orion.attribute('hasOne', {
+		label: 'Course Enrolled'
+	}, {
+		collection: Courses,
+		titleField: 'course_name',
+		publicationName: 'postCourses',
+
+		create: function (input, cb) {
+			var city = Course.insert({
+				name: input,
+				published: false
+			});
+			var newCity = Courses.findOne(course);
+			cb(newCourse);
+		}
 
 
 
-  }),
-  /**
-   * The file attribute is a custom orion attribute
-   * This is where orion do the magic. Just set 
-   * the attribute type and it will automatically
-   * create the form for the file.
-   * WARNING: the url of the image will not be saved in
-   * .image, it will be saved in .image.url.
-   */
-  /**
-   * This attribute sets the user id of the user that created 
-   * this post automatically.
-   */
-  createdBy: orion.attribute('createdBy'),
-  createdAt: orion.attribute('createdAt')
+	}),
+	/**
+	 * The file attribute is a custom orion attribute
+	 * This is where orion do the magic. Just set 
+	 * the attribute type and it will automatically
+	 * create the form for the file.
+	 * WARNING: the url of the image will not be saved in
+	 * .image, it will be saved in .image.url.
+	 */
+	/**
+	 * This attribute sets the user id of the user that created 
+	 * this post automatically.
+	 */
+	createdBy: orion.attribute('createdBy'),
+	createdAt: orion.attribute('createdAt')
 }));
 
 
@@ -101,8 +131,9 @@ Students.attachSchema(new SimpleSchema({
  */
 
 Students.helpers({
-  getCreator: function () {
-    return Meteor.users.findOne({ _id: this.createdBy });
-  }
+	getCreator: function () {
+		return Meteor.users.findOne({
+			_id: this.createdBy
+		});
+	}
 });
-
